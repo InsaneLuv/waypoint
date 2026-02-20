9cfc1887762b1efeaf8efda475e59172e8bbf4a1
luv <69699689+InsaneLuv@users.noreply.github.com>
2026-02-20 23:25:44 +0400

    Рефакторинг

--- app/deps/session.py
+++ app/deps/session.py
@@ -1,6 +1,5 @@
-from redis.asyncio import Redis
-
 from dishka import Provider, Scope, provide
+from redis.asyncio import Redis
 
 from app.repositories.redis_session_repository import RedisSessionRepository
 from app.repositories.session_repository import SessionRepository
@@ -12,24 +11,24 @@ class SessionServiceProvider(Provider):
 
     @provide(scope=Scope.REQUEST)
     def get_redis_session_repository(
-        self,
-        redis: Redis,
+            self,
+            redis: Redis,
     ) -> RedisSessionRepository:
         """Создать Redis репозиторий."""
         return RedisSessionRepository(redis)
 
     @provide(scope=Scope.REQUEST)
     def get_session_repository(
-        self,
-        redis_repo: RedisSessionRepository,
+            self,
+            redis_repo: RedisSessionRepository,
     ) -> SessionRepository:
         """Получить репозиторий сессий."""
         return redis_repo
 
     @provide(scope=Scope.REQUEST)
     def get_session_service(
-        self,
-        repository: SessionRepository,
+            self,
+            repository: SessionRepository,
     ) -> SessionService:
         """Создать сервис управления сессиями."""
         return SessionService(repository)
